c0d6c45f65f62d569b7576e70c5ad97dfd85647b
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-06-17 16:25:23 -0400

    About command added

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,12 @@ module.exports = {
 	execute(message, args) {
 
         message.channel.send("Kappa bot best bot, mean.. PONG!")
+	},
+	name: 'about',
+	description: '',
+	cooldown: 0,
+	execute(message, args) {
 
+		message.channel.send("This bot connects with the IGDB to give information about video game releases! You can contribute to this repo on https://github.com/AndyTheNerd/GameReleaseBot")
 	},
 };
