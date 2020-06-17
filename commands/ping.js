595c316199da0258efd420dd5df8d779ccc8c4db
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-06-17 16:43:26 -0400

    Fix

--- commands/ping.js
+++ commands/ping.js
@@ -6,13 +6,6 @@ module.exports = {
 	cooldown: 0,
 	execute(message, args) {
 
-        message.channel.send("Kappa bot best bot, mean.. PONG!")
-	},
-	name: 'about',
-	description: '',
-	cooldown: 0,
-	execute(message, args) {
-
-		message.channel.send("This bot connects with the IGDB to give information about video game releases! You can contribute to this repo on https://github.com/AndyTheNerd/GameReleaseBot")
+        message.channel.send("Kappa bot best bot, mean.. PONG!!")
 	},
 };
