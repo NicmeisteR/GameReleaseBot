e6952473ea65db3a9c6f82e5268573428dfd025b
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-06-17 17:27:28 -0400

    Ping test

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 module.exports = {
-	name: 'ping',
-	description: '',
-	cooldown: 0,
-	execute(message, args) {
+    name: 'ping',
+    description: '',
+    cooldown: 0,
+    execute(message, args) {
 
         message.channel.send("Kappa bot best bot, mean.. PONG!!")
-	},
+    },
 };
